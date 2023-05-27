273874c13d0461f6518fbdb421e62a11c2ae5456
Syed Fawad Hashmi <59658569+syedfawadhashmi5@users.noreply.github.com>
2023-05-27 17:18:29 +0500

    dsfsdfdf

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,26 +28,6 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-/*------------------
-		Hero Slider
-	--------------------*/
-
-  $(document).ready(function() {
-    $('.hero__slider').owlCarousel({
-      loop: true,
-      dots: true,
-      mouseDrag: false,
-      navigation : true,
-      animateOut: 'fadeOut',
-      animateIn: 'fadeIn',
-      items: 1,
-      margin: 0,
-      smartSpeed: 1200,
-      autoHeight: false,
-      autoplay: true,
-  });
-});
-
 
 
 
@@ -212,46 +192,22 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 
+  /*------------------
+		Hero Slider
+	--------------------*/
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDKkOYsXyAz7l-onRDOTYS0RwVGx8Vk4n8",
-  authDomain: "studentwebapp-f1849.firebaseapp.com",
-  projectId: "studentwebapp-f1849",
-  storageBucket: "studentwebapp-f1849.appspot.com",
-  messagingSenderId: "281704954231",
-  appId: "1:281704954231:web:dda82b779c11684514c972",
-  measurementId: "G-BDWXP5CJ8Y"
-};
- // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-var db = firebase.firestore();
-var collectionRef = db.collection("students");
-
-function submitForm() {
-  var form = document.querySelector('.signup-form');
-  var firstName = document.getElementById("first_name").value;
-  var lastName = document.getElementById("last_name").value; // fixed typo
-  var email = document.getElementById("email").value;
-  var gender = document.getElementById("male").checked ? "male" : document.getElementById("female").checked ? "female" : document.getElementById("other").checked ? "other" : "";
-  var maritalStatus = document.getElementById("single").checked ? "single" : document.getElementById("married").checked ? "married" : ""; // fixed the input value to match the checkbox IDs
-  var profilePicture = document.getElementById("file").value;
-  var age = document.getElementById("age").value;
-  var bio = document.getElementById("bio").value; // fixed ID typo
-
-  collectionRef.add({
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    gender: gender,
-    maritalStatus: maritalStatus,
-    profilePicture: profilePicture,
-    age: age,
-    bio: bio
-  })
-  .then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-    // Clear the form
-    form.reset();
- 
-  })}
+  $(document).ready(function() {
+    $('.hero__slider').owlCarousel({
+      loop: true,
+      dots: true,
+      mouseDrag: false,
+      navigation : true,
+      animateOut: 'fadeOut',
+      animateIn: 'fadeIn',
+      items: 1,
+      margin: 0,
+      smartSpeed: 1200,
+      autoHeight: false,
+      autoplay: true,
+  });
+});
